22335f30dee620ef4d93789f233dd8c5e019ae23
skellybellie <skellybellie@gmail.com>
2021-09-16 19:51:13 -0500

    Linked screens
    Got main menu screen linked to lobby screen and lobby screen linked to character setup.

--- Amongst Them Unity/Assets/Scripts/GoToLobby.cs
+++ Amongst Them Unity/Assets/Scripts/GoToLobby.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GoToLobby : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        public void ChangeScene(string SceneName)
+        {
+        Debug.Log("Hey Fuckers");
+             SceneManager.LoadScene(SceneName);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
         
-    }
 }
